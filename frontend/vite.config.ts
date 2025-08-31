9205002dde5bbd79cca50b48c9ead8547adfe774
Plutoochu <eldarmuratovic710@gmail.com>
2025-08-31 21:14:41 +0200

    Prepravka vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+  },
   server: {
     port: 3000,
     open: true,
